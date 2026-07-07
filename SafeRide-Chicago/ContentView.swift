486a6891e792583c8b98e32a497e30b7cfe67f2d
jskariah <justin.skariah9@gmail.com>
2026-07-07 10:10:13 -0500

    Initial Commit

---
+++ SafeRide-Chicago/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SafeRide-Chicago
+//
+//  Created by 6 BGCC Loan Library on 7/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
