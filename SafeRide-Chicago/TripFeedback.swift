69c3c8b84ea5c31034a7709ce603114602ea4ddc
justinskariah28 <jskar@uic.edu>
2026-07-08 10:14:01 -0500

    Changes

---
+++ SafeRide-Chicago/TripFeedback.swift
@@ -0,0 +1,61 @@
+//
+//  TripFeedback.swift
+//  SafeRide-Chicago
+//
+//  Created by 30 BGCC Loan Library on 7/8/26.
+//
+
+//
+//  ContentView.swift
+//  SafeRide-Chicago
+//
+//  Created by 6 BGCC Loan Library on 7/7/26.
+//
+
+import SwiftUI
+
+struct TripFeedback: View {
+    var body: some View {
+        
+        VStack {
+            //Image(systemName: "globe")
+               // .imageScale(.large)
+               // .foregroundStyle(.tint)
+            
+            Text("Trip Feedback")
+                .font(.title)
+
+               Text("Please rate your trip.")
+                   .padding(.top, 40)
+
+               Text("Did this trip meet your accessibility needs?")
+                
+                @State var selectedAnswer = ""
+                VStack{
+                    Button("Yes") {
+                         selectedAnswer = "Yes"
+                     }
+
+                     Button("Maybe") {
+                         selectedAnswer = "Maybe"
+                     }
+
+                     Button("No") {
+                         selectedAnswer = "No"
+                     }
+                }
+               Text("Provide any additional comments below.")
+             
+               Text("Thank you for using SafeRoute Chicago!")
+                .padding(.top, 475)
+            
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .padding()
+    }
+   
+}
+
+#Preview {
+    ContentView()
+}
